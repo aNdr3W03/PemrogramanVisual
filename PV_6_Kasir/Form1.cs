6bee8eec4f2003a6a3335f6aa041d1bb55749f54
aNdr3W03 <andrewbjamesie@yahoo.com>
2022-03-20 03:52:21 +0700

    Add files via upload
    PV Tugas Pertemuan 6

---
+++ PV_6_Kasir/Form1.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.Data.SqlClient;
+
+namespace PV_6_Kasir
+{
+    public partial class Form1 : Form
+    {
+        private SqlCommand cmd;
+        private DataSet ds;
+        private SqlDataAdapter da;
+
+        Koneksi Konn = new Koneksi();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContectClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SqlConnection conn = Konn.GetConn();
+
+            try
+            {
+                conn.Open();
+                cmd = new SqlCommand("SELECT * FROM TBL_BARANG", conn);
+                ds = new DataSet();
+                da = new SqlDataAdapter(cmd);
+                da.Fill(ds, "TBL_BARANG");
+                dataGridView1.DataSource = ds;
+                dataGridView1.DataMember = "TBL_BARANG";
+                dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            }
+            catch (Exception G)
+            {
+                MessageBox.Show(G.ToString());
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+    }
+}
