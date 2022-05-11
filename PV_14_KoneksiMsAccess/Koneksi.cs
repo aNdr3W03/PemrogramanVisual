47ee398f0e43d2a118274ed833b4ba7c9d31874f
aNdr3W03 <andrewbjamesie@yahoo.com>
2022-05-12 01:31:14 +0700

    Add files via upload
    PV Tugas Pertemuan 14 (1)

---
+++ PV_14_KoneksiMsAccess/Koneksi.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.OleDb;
+
+namespace PV_14_KoneksiMsAccess
+{
+    internal class Koneksi
+    {
+        public OleDbConnection GetConn()
+        {
+            OleDbConnection conn = new OleDbConnection();
+            conn.ConnectionString = "Provider = Microsoft.Jet.OLEDB.4.0; Data Source=|DataDirectory|/DBBarang.mdb";
+            return conn;
+        }
+    }
+}
