30bf2ef42b560f8a0f7cda3c2fa7b38470dc907c
aNdr3W03 <andrewbjamesie@yahoo.com>
2022-05-12 01:27:38 +0700

    Add files via upload
    PV Tugas Pertemuan 12 (1)

---
+++ PV_12_Kasir-KodeAutomatic/Koneksi.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.SqlClient;
+
+namespace PV_12_KasirKodeAutomatic
+{
+    internal class Koneksi
+    {
+        public SqlConnection GetConn()
+        {
+            SqlConnection Conn = new SqlConnection();
+            Conn.ConnectionString = "Data Source = ANDREW-BENEDICT; initial catalog = Kasir; integrated security = true";
+            return Conn;
+        }
+    }
+}
