6dc4daca2cdbe03b21f94e0be2ec561b112b45b1
aNdr3W03 <andrewbjamesie@yahoo.com>
2022-05-12 01:28:40 +0700

    Add files via upload
    PV Tugas Pertemuan 13 (1)

---
+++ PV_13_Kasir-TextBoxValidation/Koneksi.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.SqlClient;
+
+namespace PV_13_KasirTextBoxValidation
+{
+    internal class Koneksi
+    {
+        public SqlConnection GetConn()
+        {
+            SqlConnection Conn = new SqlConnection();
+            Conn.ConnectionString = "Data Source = ANDREW-BENEDICT; initial catalog = Kasir; integrated security = true";
+            return Conn;
+        }
+    }
+}
